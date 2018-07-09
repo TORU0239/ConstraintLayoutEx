cad50867fc3925be9c1faf490c34775cb13e7a18
Toru Choi <toru.choi@11street.my>
2018-07-09 18:05:37 +0800

    example of chain

---
+++ app/src/main/java/my/com/toru/constraintlayoutex/ChainEx.kt
@@ -0,0 +1,13 @@
+package my.com.toru.constraintlayoutex
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ChainEx : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.elements_chain)
+        title = "ChainEx"
+    }
+}
--- app/src/main/java/my/com/toru/constraintlayoutex/MainActivity.kt
+++ app/src/main/java/my/com/toru/constraintlayoutex/MainActivity.kt
@@ -1,16 +1,24 @@
 package my.com.toru.constraintlayoutex
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.elements_for_constraint.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.elements_for_constraint)
-//        setContentView(R.layout.activity_main)
-//        text.setOnClickListener {
-//            startActivity(Intent(this@MainActivity, NextActivity::class.java))
-//        }
+        btn_chain.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ChainEx::class.java))
+        }
+
+        btn_guide.setOnClickListener {
+
+        }
+
+        btn_dimension.setOnClickListener {
+        }
     }
 }
